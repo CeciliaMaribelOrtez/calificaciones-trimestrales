4e6d8297bab46fe53c66c55c780518675fb9d037
CeciliaMaribelOrtez <CeciliaMaribelOrtez@users.noreply.github.com>
2023-02-12 01:03:08 +0000

    deploy: f6c4053fa469cb1ca3ae618cb3f80213598d3139

---
+++ assets/login-2b9388a1.js
@@ -0,0 +1,1 @@
+import{l as o}from"./auth-f66db6b2.js";const t=document.querySelector("#btn-google");t.addEventListener("click",e=>{e.preventDefault(),o()});
--- assets/login-2edf16f5.js
+++
@@ -1,1 +0,0 @@
-import{l as o}from"./auth-f66db6b2.js";/* empty css                */const t=document.querySelector("#btn-google");t.addEventListener("click",e=>{e.preventDefault(),o()});
